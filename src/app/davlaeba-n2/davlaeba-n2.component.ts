964c24fe63233adf08ae70a22990eb4acba502ef
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2024-03-11 21:00:41 +0400

    meorec morcha da amis dedasdklasd

--- src/app/davlaeba-n2/davlaeba-n2.component.ts
+++ src/app/davlaeba-n2/davlaeba-n2.component.ts
@@ -12,4 +12,25 @@ export class DavlaebaN2Component implements OnInit {
   ngOnInit(): void {
   }
 
+  ngColor = {};
+  colorClass = {}
+  redClass = 'redClass';
+  isHovered = false;
+  greenClass = 'greenClass'
+  yellowClass = 'yellowClass'
+  cyanClass = 'cyanClass'
+  
+  setHover(hoverChange: boolean) {
+    this.isHovered = hoverChange;
+    console.log(this.isHovered)
+    if(this.isHovered) {
+      this.colorClass = this.redClass
+    } else {
+      this.colorClass = {}
+    }
+  }
+
+  changeHover(color: any) {
+    this.redClass = color
+}
 }
